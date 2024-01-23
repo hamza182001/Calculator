1b561e819bd235d16ee4c9892ba0827d9983e5c9
mohammad hamza <hamzarvp20wr10@gmail.com>
2024-01-23 14:37:13 +0500

    adding ui and class for calculator

---
+++ calculator.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Calculator {
+private:
+    float num1;
+    float num2;
+
+public:
+    Calculator();
+    Calculator(float num1,float num2) {
+        this->num1 = num1;
+        this->num2 = num2;
+    }
+    float addition();
+    float subtraction();
+    float multiplication();
+    float division();
+};
+
+
+int main() {
+    Calculator calculator; 
+
+    int choice = 0;
+    cout << "Enter which operation you would like to do" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        calculator.addition();
+        break;
+    case 2:
+        calculator.subtraction();
+        break;
+    case 3:
+        calculator.multiplication();
+        break;
+    case 4:
+        calculator.division();
+        break;
+    default:
+        cout << "Invalid choice" << endl;
+    }
+
+    return 0;
+}
